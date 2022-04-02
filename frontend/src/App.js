aeeae1f98567f68ebe011773906c19fd46b60ddf
Rodolfo Morales <rodolfomlphx@gmail.com>
2022-04-02 16:19:56 -0700

    added react router dom and link tags instead of a tags

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,23 @@
 import './App.css';
 import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Header />
       <main className='py-3'>
-        <HomeScreen />
+        <Routes>
+          <Route path='/' element={<HomeScreen />} exact />
+          <Route path='/product/:id' element={<ProductScreen />}  />
+        </Routes>
       </main>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
